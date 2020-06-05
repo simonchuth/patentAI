fdc20a319e2a3f4c469566a970ba7cea8459ec03
Simon Chu <simonchuth@gmail.com>
2020-06-05 10:49:12 +0800

    Add functions to process claims, intro and definition

--- src/datapipeline/preprocess_ipos_api.py
+++ src/datapipeline/preprocess_ipos_api.py
@@ -4,6 +4,7 @@ import pickle
 import requests, PyPDF2, io
 from multiprocessing import Process, Manager, cpu_count
 import argparse
+import re
 
 
 def fetch_patent_url(numdays=3650):
@@ -25,6 +26,7 @@ def fetch_patent_url(numdays=3650):
                     target_doc.append(doc['url'])
     return target_doc
 
+
 def chunk_doc(target_doc, num_worker):
     chunk_size = int(len(target_doc) / num_worker) + 1
     chunklist = [target_doc[x:x + chunk_size] for x in range(0, len(target_doc), chunk_size)]
@@ -40,18 +42,40 @@ def load_web_pdf(url):
         txt = [read_pdf.getPage(i).extractText() for i in range(num_pages)]
         return txt
     
+
 def extract_intro(txt):
     intro = txt[0:2]
     intro = ' '.join(intro)
     return intro
-    
+
+
 def extract_claim_text(txt):
     claims_start_page = [i for i, page in enumerate(txt) if ('CLAIMS' in page) or ('what is claimed is' in page.lower())][0]
     claim_pages = txt[claims_start_page:]
     text_pages = txt[0:claims_start_page]
     return claim_pages, text_pages
 
-def main_extraction(L, target_doc):
+
+def process_definition(text_pages, definition_pattern=r'[Tt]he term ".+?".+?\.'):
+    pages = [page.replace('\n', '')[1:].strip() for page in text_pages]
+    full_text = ' '.join(pages)
+    definitions = re.findall(definition_pattern, full_text)
+    return definitions
+
+
+def process_claim(claim_pages, claim_pattern = r'\d+?\..+?\.'):
+    pages = [page.replace('\n', '')[1:].strip() for page in claim_pages]
+    calim_full_text = ' '.join(pages)
+    claims = re.findall(claim_pattern, calim_full_text)
+    return claims
+
+
+def process_intro(intro_pages):
+    intro_text = intro_pages.replace('\n', '')
+    return intro_text
+
+
+def main_extraction(target_doc, L=None):
     access_problem = []
     data = []
     failed_extract_text = []
@@ -72,7 +96,10 @@ def main_extraction(L, target_doc):
         except Exception:
             failed_extract_text.append(txt)
     output = [data, failed_extract_text, access_problem]
-    L.append(output)
+    if L is None:
+        return output  # Normal usage
+    else:
+        L.append(output)  # Multiprocessing
     print(f'Completed {len(L)} chunks')
 
 
