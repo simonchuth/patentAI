d50bd075afbe685ebae79e4fcc47568e7099fbcd
Simon Chu <simon_chu@kelaberetiv.sg>
2020-06-06 16:09:40 +0800

    Fix bug with args.max_length default val

--- src/pipeline/encode_dict.py
+++ src/pipeline/encode_dict.py
@@ -11,10 +11,9 @@ if __name__ == "__main__":
     parser.add_argument("--unique_word_pkl", default=None)
     parser.add_argument("--savepath", default=None)
     parser.add_argument("--vocab_size", type=int, default=1000000)
-    parser.add_argument("--max_length", type=int, default=None)
+    parser.add_argument("--max_length", type=int, default=50)
 
     args = parser.parse_args()
-
     unique_word = pickle_load(args.unique_word_pkl)
     print('Encoding')
     vocab_dict = encode_dict(unique_word,
