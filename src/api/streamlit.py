b1428eb61c7e3dd5e4acaf84df7a35331b66fec2
Simon Chu <simon_chu@kelaberetiv.sg>
2020-06-06 22:36:16 +0800

    basic web UI working prototype

--- src/api/streamlit.py
+++ src/api/streamlit.py
@@ -5,16 +5,28 @@ import json
 
 from os.path import join
 
-st.title('Singlish Sentence Classifier')
+st.title('PatentAI')
 
-api_base = st.text_input('Please enter path to the Sentence Classifier API',
+api_base = st.text_input('Please enter path to the PatentAI API',
                          'http://192.168.1.111:8000/')
 
 try:
     connection_check = requests.get(api_base)
-    st.success('Connected to Sentence Classifier API successfully')
+    st.success('Connected to PatentAI API successfully')
 
 except Exception:
-    st.warning('Connection to Sentence Classifier API cannot be established, \
+    st.warning('Connection to PatentAI API cannot be established, \
                 please check the path')
     st.button('Refresh')
+
+intro = st.text_area('Paste the introduction here')
+claim = st.text_area('Paste the claims here')
+term = st.text_input('Please enter the term here')
+
+if st.button('Submit'):
+    predict_api = join(api_base, 'predict')
+    inputs = {'claim': claim, 'intro': intro, 'term': term}
+    with st.spinner("Generating definition..."):
+        result = requests.post(predict_api, data=json.dumps(inputs)).json()
+    st.success('Definition generation completed')
+    st.write(result['Result'])
