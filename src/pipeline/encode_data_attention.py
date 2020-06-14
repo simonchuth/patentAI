0747f559ef62effe5bd1bc937d3f9e6cf368ecec
Simon Chu <simon_chu@kelaberetiv.sg>
2020-06-14 23:40:20 +0800

    initial commit of new pipeline

---
+++ src/pipeline/encode_data_attention.py
@@ -0,0 +1,45 @@
+import argparse
+
+import gc
+
+from src.utils.general import pickle_load
+from src.utils.general import pickle_save
+from src.utils.general import join_path
+from src.utils.general import check_mkdir
+
+from src.utils.encode import encode_attention_app
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--data_folder", default=None)
+    parser.add_argument("--mode", default='attention')
+
+    args = parser.parse_args()
+
+    extracted_pkl = join_path(args.data_folder, ['extracted_txt',
+                                                 'extracted.pkl'])
+    tensor_folder = join_path(args.data_folder, ['tensor',
+                                                 args.mode])
+    params_path = join_path(args.data_folder, 'params.pkl')
+
+    check_mkdir(tensor_folder)
+
+    dataset = pickle_load(extracted_pkl)
+
+    for i, app in enumerate(dataset):
+        output = encode_attention_app(dataset)
+        for j, definition in enumerate(output[2]):
+            save_list = [output[0],
+                         output[1],
+                         definition[0],
+                         definition[1],
+                         definition[2]]
+            train_name = f'{i}_{j}_train.pkl'
+            train_path = join_path(tensor_folder, train_name)
+            pickle_save(save_list, train_path)
+            del output
+            gc.collect()
+
+    print('Completed')
